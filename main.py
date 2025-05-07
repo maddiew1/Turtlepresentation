648356e1cbf7e286deffd8b0d5b029fa81f962d2
28wodarekm <28wodarekm@bwschools.net>
2025-05-07 09:25:34 -0400

    Commit

--- main.py
+++ main.py
@@ -3,141 +3,310 @@ import random
 
 screen = turtle.Screen()
 screen.screensize(500,500)
-screen.bgcolor('red')
+screen.bgcolor('purple')
 t = turtle.Turtle()
-t2 = turtle.Turtle()
-t3 = turtle.Turtle()
-t2.hideturtle()
-t3.hideturtle()
-FORCE_FACTOR = 30
-
-#intro screen
-t.penup()
-t.goto(0, 100)
-t.pendown()
-t.write('', font=("ariel", 30, "italic"), align="center")
 
 t.penup()
-t.goto(0, 0)
+t.goto(0,0)
 t.pendown()
-screen.addshape('turtle.gif')
-t.shape('turtle.gif')
+t.write('Maddie Wodarek', font=("ariel", 30, "italic"), align="center")
+
+t.penup()
+t.goto(200,200)
+t.pendown()
+
+t.pencolor('black')
+t.fillcolor('peachpuff2')
+t.begin_fill()
+t.goto(100,200)
+t.goto(150,300)
+t.goto(200,200)
+t.end_fill()
+t.setheading(0)
+
+t.penup()
+t.goto(-50,-200)
+t.pendown()
+screen.addshape('girl (1).gif')
+t.shape('girl (1).gif')
 t.stamp()
 t.shape('classic')
 
-
 t.penup()
-t.goto(0, -100)
+t.goto(60,-200)
 t.pendown()
+screen.addshape('flower.gif')
+t.shape('flower.gif')
+t.stamp()
+t.shape('classic')
+t.penup()
+t.goto(0,-350)
+t.pendown()
+t.write('To go to the next page, press enter', font=("ariel", 30, "italic"), align="center")
 
-
-t.write('Miss', font=("ariel", 30, "italic"), align="center")
 round = input("Press Enter to Continue: ")
+
 t.clear()
-screen.bgcolor('tan')
-
-hit = 0
-attempt = 1
-while attempt <= 5:
-    t2.penup()
-    t2.goto(0, 200)
-    t2.pendown()
-
-    t.setheading(0)
-    #variables
-    target_x = random.randint(50,170)
-    sign_x = random.randint(1,2)
-    if sign_x == 2:
-        target_x = random.randint(-250,-130)
-    TARGET_L_LEFT_X = target_x
-    TARGET_L_LEFT_Y = random.randint(-250, 170)
-    SIDES = random.randint(20,80)
-    FORCE_FACTOR= 30
-    hit = 0
-    attempt = 2
-
-
-    t.penup()
-    t.goto(TARGET_L_LEFT_X, TARGET_L_LEFT_Y)
-    t.pendown()
-
-    t.forward(SIDES)
-    t.left(90)
-    t.forward(SIDES)
-    t.left(90)
-    t.forward(SIDES)
-    t.left(90)
-    t.forward(SIDES)
-    t.left(90)
-
-    t.penup()
-    t.goto(0,0)
-    t.pendown()
-
-    t.setheading(0)
-
-    angle = float(input("Enter the angle: "))
-    force = float(input("Enter a force (1-10): "))
-
-    distance = force*FORCE_FACTOR
-
-    t.setheading(angle)
-    t.penup()
-    t.speed(1)
-    t.forward(distance)
-    t.speed(0)
-
-    if (t.xcor() >= TARGET_L_LEFT_X and t.ycor() <= TARGET_L_LEFT_Y + SIDES) and (t.ycor() >= TARGET_L_LEFT_Y and t.ycor() <= TARGET_L_LEFT_Y + SIDES):
-        t2.write("Hit", font =("ariel", 30, "italic"),align = "center")
-        hit += 1
-
-    else:
-        t2.write("Miss", font =("ariel", 30, "italic"),align = "center")
-    t3.penup()
-    t3.goto(0,150)
-    t3.pendown()
-    t3.clear()
-    t3.write(str(hit)+"/"+str(attempt), font =("ariel", 30, "italic"),align = "center")
-
-    round = input("Press Enter to Continue: ")
-    t.clear()
-    t2.clear()
-    t3.penup()
-    t3.goto(0, -100)
-    t3.pendown()
-    hit = 1
-    if attempt == 5:
-        if hit >=4:
-            t.write('Sniper', font=("ariel", 30, "italic"), align="center")
-        elif hit >= 2:
-            t.write('Pretty Good, Keep Practicing', font=("ariel", 30, "italic"), align="center")
-        else:
-            t.write('Practice, Practice, Practice', font=("ariel", 30, "italic"), align="center")
-
-    round = input("Press Enter to Continue: ")
-    attempt = attempt + 1
-#End Screen
-t.clear()
-t2.clear()
-t3.clear()
-screen.bgcolor('dodgerblue')
+screen.bgcolor('gray')
 t.penup()
-t.goto(0, 100)
+t.goto(0,0)
 t.pendown()
-t.write('Game', font=("ariel", 30, "italic"), align="center")
+t.write('Favorite Foods', font=("ariel", 30, "italic"), align="center")
+t.penup()
+t.goto(-200,0)
+t.pendown()
+t.pencolor('black')
+t.fillcolor('mediumseagreen')
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.end_fill()
 
 t.penup()
-t.goto(0, 0)
+t.goto(-170,-200)
 t.pendown()
-screen.addshape('turtlefiveleg.gif')
-t.shape('turtlefiveleg.gif')
+screen.addshape('salad.gif')
+t.shape('salad.gif')
 t.stamp()
 t.shape('classic')
 
+t.penup()
+t.goto(-170,-300)
+t.pendown()
+t.write('Chicken Salad', font=("ariel", 15, "italic"), align="center")
 
 t.penup()
-t.goto(0, -100)
+t.goto(-20,-200)
 t.pendown()
-t.write('Over', font=("ariel", 30, "italic"), align="center")
+screen.addshape('fruit.gif')
+t.shape('fruit.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(-20,-300)
+t.pendown()
+t.write('Fruit', font=("ariel", 15, "italic"), align="center")
+
+
+t.penup()
+t.goto(100,-200)
+t.pendown()
+screen.addshape('alfredo.gif')
+t.shape('alfredo.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100,-300)
+t.pendown()
+t.write('Alfredo Pasta', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(0,-350)
+t.pendown()
+t.write('To go to the next page, press enter', font=("ariel", 30, "italic"), align="center")
+
+
 round = input("Press Enter to Continue: ")
+t.clear()
+screen.bgcolor('skyblue')
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write('Hobbies', font=("ariel", 30, "italic"), align="center")
+
+t.penup()
+t.goto(0,-250)
+t.pendown()
+t.pencolor('pink')
+t.begin_fill()
+t.fillcolor('pink')
+t.begin_fill()
+t.circle(50)
+t.setheading(270)
+t.end_fill()
+t.pencolor('black')
+
+
+
+t.penup()
+t.goto(-190,200)
+t.pendown()
+screen.addshape('volleyball.gif')
+t.shape('volleyball.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(-190,300)
+t.pendown()
+t.write('Playing Volleyball', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(-65,200)
+t.pendown()
+screen.addshape('music (1).gif')
+t.shape('music (1).gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(-65,100)
+t.pendown()
+t.write('Listening to Music', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(60,200)
+t.pendown()
+screen.addshape('dog.gif')
+t.shape('dog.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(60,300)
+t.pendown()
+t.write('Walking my Dog', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(200,200)
+t.pendown()
+screen.addshape('friends.gif')
+t.shape('friends.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(190,120)
+t.pendown()
+t.write('Hanging out with my Friends', font=("ariel", 10, "italic"), align="center")
+
+t.penup()
+t.goto(0,-350)
+t.pendown()
+t.write('To go to the next page, press enter', font=("ariel", 30, "italic"), align="center")
+
+round = input("Press Enter to Continue: ")
+
+
+t.clear()
+screen.bgcolor('pink')
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write('Favorite Movies', font=("ariel", 30, "italic"), align="center")
+t.penup()
+t.goto(200,200)
+t.pendown()
+t.pencolor('black')
+t.begin_fill()
+t.fillcolor('purple')
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.goto(200,200)
+t.end_fill()
+
+t.penup()
+t.goto(-100,-200)
+t.pendown()
+screen.addshape('notebook.gif')
+t.shape('notebook.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(-100,-300)
+t.pendown()
+t.write('The Notebook', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(100,-200)
+t.pendown()
+screen.addshape('itendswithus.gif')
+t.shape('itendswithus.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100,-100)
+t.pendown()
+t.write('It Ends With Us', font=("ariel", 15, "italic"), align="center")
+
+t.penup()
+t.goto(0,-350)
+t.pendown()
+t.write('To go to the next page, press enter', font=("ariel", 30, "italic"), align="center")
+
+round = input("Press Enter to Continue: ")
+
+t.clear()
+screen.bgcolor('moccasin')
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write('Favorite Sport', font=("ariel", 30, "italic"), align="center")
+t.penup()
+t.goto(150,200)
+t.pendown()
+t.setheading(45)
+t.fillcolor("purple")
+t.begin_fill()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.end_fill()
+t.hideturtle()
+
+t.penup()
+t.goto(-100,-200)
+t.pendown()
+screen.addshape('volleyball.gif')
+t.shape('volleyball.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100,-200)
+t.pendown()
+screen.addshape('volleyballnet.gif')
+t.shape('volleyballnet.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(0,-130)
+t.pendown()
+t.write('Volleyball', font=("ariel", 20, "italic"), align="center")
+
+t.penup()
+t.goto(0,-350)
+t.pendown()
+t.write('The End!', font=("ariel", 30, "italic"), align="center")
+
+
+
+
+
+
+
+
+
+
 turtle.done()
